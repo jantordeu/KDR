f240f9ac0410c0be4520d414200b4b5d317d0312
jantordeu <htordeux@gmail.com>
2021-03-03 18:48:43 +0100

    update hekili

--- rotations/shaman_elemental.lua
+++ rotations/shaman_elemental.lua
@@ -1,23 +1,15 @@
 --[[[
 @module Shaman Elemental Rotation
-@author kirk24788
-@version 8.0.1
+@author Kirk24788
+@version Hekili
 ]]--
 local spells = kps.spells.shaman
 local env = kps.env.shaman
 
 
-kps.rotations.register("SHAMAN","ELEMENTAL", {
-    {{"nested"}, 'kps.defensive', {
-        {spells.astralShift, 'player.hp < 0.5'},
-        {spells.healingSurge, 'player.hp < 0.7'},
-    }},
-    {spells.flameShock, 'not target.hasMyDebuff(spells.flameShock)'},
-    {spells.earthElemental, 'kps.cooldowns'},
-    {spells.fireElemental, 'kps.cooldowns'},
-    {spells.earthShock, 'player.maelstrom >= 60'},
-    {spells.frostShock, 'player.isMoving'},
-    {spells.lavaBurst,  'target.hasMyDebuff(spells.flameShock)'},
-    {spells.lightningBolt},
+kps.rotations.register("SHAMAN","ELEMENTAL",
+{
+    {kps.hekili({
+    })}
 }
-,"Icy Veins Easy Mode")
+,"Hekili")
